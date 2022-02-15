52c28665c5b3c0cd044bfdd2d4f75044cd52b9da
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-02-15 09:44:01 +0600

    Created `index.js`

---
+++ client/src/assets/index.js
@@ -0,0 +1,11 @@
+const assets = {
+    images: {
+        logo: require('./images/kanban.png'),
+        appIcon: require('./images/kanban.png'),
+    },
+    colors: {
+        secondary: "#292929"
+    }
+}
+
+export default assets;
