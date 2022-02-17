246c8ddd6bfc68daf536d7cc468ddbb7d8014154
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-02-18 01:14:17 +0600

    Created `App.jsx`

---
+++ client/src/App.jsx
@@ -0,0 +1,42 @@
+import "@fontsource/poppins/300.css";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/700.css";
+import './css/custom-scrollbar.css'
+import CssBaseLine from '@mui/material/CssBaseline'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import AppLayout from './components/layout/AppLayout'
+import AuthLayout from './components/layout/AuthLayout'
+import Home from './pages/Home'
+import Board from './pages/Board'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
+
+function App() {
+    const theme = createTheme({
+        palette: { mode: 'dark' }
+    })
+
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseLine />
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<AuthLayout />}>
+                        <Route path='login' element={<Login />} />
+                        <Route path='signup' element={<Signup />} />
+                    </Route>
+
+                    <Route path='/' element={<AppLayout />}>
+                        <Route index element={<Home />} />
+                        <Route path='boards' element={<Home />} />
+                        <Route path='boards/:boardId' element={<Board />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </ThemeProvider>
+    );
+}
+
+export default App;
