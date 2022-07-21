91682833433cbb6a25b514c0287a9e8dc0d9247b
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-07-22 00:52:32 +0600

    :sparkles: App Config Change

--- server/app.ts
+++ server/app.ts
@@ -3,8 +3,9 @@ import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 import cors from "cors";
+import route from "@routes"
 
-const app = express();
+const app = express()
 
 app.use(cors())
 app.use(logger('dev'));
@@ -12,6 +13,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/api/v1', require('./src/routes'));
+app.use('/api/v1', route);
 
 module.exports = app;
